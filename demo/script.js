cf040358f6aa29913973ee1e83763a171fc9b597
tsaruggan <tsaruggan@gmail.com>
2024-07-17 23:32:30 -0400

    refactored

--- demo/script.js
+++ demo/script.js
@@ -1,17 +1,46 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { format, addMinutes, differenceInMinutes } from 'date-fns';
+import { format, addMinutes } from 'date-fns';
 
-// Constants
+// Constants & Global Variables
+
+// Three.js components
+let scene;
+let camera;
+let renderer;
+let geometry;
+let material;
+let directionalLight;
+let controls;
+let moon;
+
+let fileLoader = new THREE.FileLoader();
+let textureLoader = new THREE.TextureLoader();
+
+const TEXTURE_MAP_PATH = '../maps/texture_map.png';
+const NORMAL_MAP_PATH = '../maps/normal_map.png';
+const VERTEX_SHADER_PATH = '../shaders/vertex_shader.glsl';
+const FRAGMENT_SHADER_PATH = '../shaders/fragment_shader.glsl';
+
 const INITIAL_SCALE_FACTOR = 0.0075;
 const INITIAL_X_ROTATION = 0;
 const INITIAL_Y_ROTATION = 0;
 const INITIAL_Z_ROTATION = 0;
 
-// Three.js components
-let scene, camera, renderer, textureLoader, geometry, texture, normalMap, material, ambientLight, directionalLight, controls, moon, currentDate, initialLibration, intervalId, resetVisibility;
+// Datetime + Phase stuff
+let currentDate;
+let initialLibration;
+let timeTravelIntervalId;
+const TIME_TRAVEL_SPEED = 1; // ms
+const TIME_TRAVEL_INCREMENT = 10; // mins
+const TIME_TRAVEL_STEP = 60; // mins
+const LIBRATION_EFFECT = 3;
+const PHASE_LIGHT_SCALE = 10;
 
-function init(shaders) {
+
+// Setup & Initialization
+
+function init(assets) {
     // Scene setup
     scene = new THREE.Scene();
 
@@ -30,126 +59,111 @@ function init(shaders) {
     document.body.appendChild(renderer.domElement);
 
     // Light setup
-    // ambientLight = new THREE.AmbientLight(0xffffff, 10);
-    // scene.add(ambientLight);
-
-    directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+    directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.castShadow = true;
     directionalLight.shadow.mapSize.width = 2048;
     directionalLight.shadow.mapSize.height = 2048;
     scene.add(directionalLight);
 
-    // Create moon
-    textureLoader = new THREE.TextureLoader();
-    texture = textureLoader.load('../maps/texture_map.png');
-    normalMap = textureLoader.load('../maps/normal_map.png');
-
-    // Sphere geometry
+    // Geometry setup
     geometry = new THREE.SphereGeometry(2, 64, 64);
 
-    // Shader material
+    // Material setup
     material = new THREE.ShaderMaterial({
         glslVersion: THREE.GLSL3,
         uniforms: {
-            textureMap: { value: texture, type: 't' },
-            normalMap: { value: normalMap, type: 't' },
+            textureMap: { value: assets.textureMap, type: 't' },
+            normalMap: { value: assets.normalMap, type: 't' },
             lightPosition: { value: directionalLight.position, type: 'v3' },
             uvScale: { type: 'v2', value: new THREE.Vector2(1.0, 1.0)}
         },
-        vertexShader: shaders.vertexShaderSource,
-        fragmentShader: shaders.fragmentShaderSource
+        vertexShader: assets.vertexShader,
+        fragmentShader: assets.fragmentShader
     });
 
-    // Create mesh
+    // Mesh setup
     moon = new THREE.Mesh(geometry, material);
     moon.geometry.computeTangents();
     moon.position.set(0, 0, 0);
-    moon.rotation.set(0, 0, 0);
+    moon.rotation.x = INITIAL_X_ROTATION;
+    moon.rotation.y = INITIAL_Y_ROTATION;
+    moon.rotation.z = INITIAL_Z_ROTATION;
     scene.add(moon);
-
 
     // Orbit controls setup
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
 
-    // Initialize current date
+    // Datetime + Phase setup
     currentDate = new Date();
     document.getElementById('dateTimePicker').value = format(currentDate, "yyyy-MM-dd'T'HH:mm");
-
-    // Initial moon appearance
     initialLibration = calculateMoonLibration(currentDate);
-    moon.rotation.x = INITIAL_X_ROTATION;
-    moon.rotation.y = INITIAL_Y_ROTATION;
-    moon.rotation.z = INITIAL_Z_ROTATION;
     moon.scale.setScalar(initialLibration.apparentSize / INITIAL_SCALE_FACTOR);
     updateScene();
 
-    // Event listeners
+    // Event listener setup
+
+    // Datetime picker event listener
     document.getElementById('dateTimePicker').addEventListener('input', (event) => {
         const dateTime = new Date(event.target.value);
         if (!isNaN(dateTime)) {
             currentDate = dateTime;
             updateScene();
         }
-        resetVisibility = true;
-        setResetVisibility();
+        disableResetButton(false);
     });
 
+    // Orbit Controls event listener
     controls.addEventListener('start', () => {
-        resetVisibility = true;
-        setResetVisibility();
+        disableResetButton(false);
     });
 
-    // Left button events
+    // Left button event listeners
     document.getElementById('leftButton').addEventListener('mousedown', () => {
-        adjustDateTime(-60);
-        intervalId = setInterval(() => adjustDateTime(-10), 1);
+        adjustDateTime(-TIME_TRAVEL_STEP);
+        timeTravelIntervalId = setInterval(() => adjustDateTime(-TIME_TRAVEL_INCREMENT), TIME_TRAVEL_SPEED);
     });
 
     document.getElementById('leftButton').addEventListener('mouseup', () => {
-        clearInterval(intervalId);
+        clearInterval(timeTravelIntervalId);
     });
 
     document.getElementById('leftButton').addEventListener('mouseleave', () => {
-        clearInterval(intervalId);
+        clearInterval(timeTravelIntervalId);
     });
 
-    // Right button events
+    // Right button event listeners
     document.getElementById('rightButton').addEventListener('mousedown', () => {
-        adjustDateTime(60);
-        intervalId = setInterval(() => adjustDateTime(10), 1);
+        adjustDateTime(TIME_TRAVEL_STEP);
+        timeTravelIntervalId = setInterval(() => adjustDateTime(TIME_TRAVEL_INCREMENT), TIME_TRAVEL_SPEED);
     });
 
     document.getElementById('rightButton').addEventListener('mouseup', () => {
-        clearInterval(intervalId);
+        clearInterval(timeTravelIntervalId);
     });
 
     document.getElementById('rightButton').addEventListener('mouseleave', () => {
-        clearInterval(intervalId);
+        clearInterval(timeTravelIntervalId);
     });
 
-    // GitHub button event
+    // GitHub button event listener
     document.getElementById('githubButton').addEventListener('click', () => {
         window.open('https://github.com/tsaruggan/moon-clock', '_blank');
     });
 
-    // Reset button event
+    // Reset button event listener
     document.getElementById('resetButton').addEventListener('click', () => {
         currentDate = new Date();
         document.getElementById('dateTimePicker').value = format(currentDate, "yyyy-MM-dd'T'HH:mm");
-        resetVisibility = false;
-        setResetVisibility();
+        disableResetButton(true)
         moon.position.set(0, 0, 0);
         controls.reset();
         updateScene();
     });
-
-    // Micellaneous variables
-    intervalId; // used for button longpress
-    resetVisibility = false; // used to display reset button
 }
 
 // Animation loop
+
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
@@ -157,12 +171,12 @@ function animate() {
 }
 
 // Helper functions
-// Function to calculate the moon phase angle
-// Function to calculate the moon phase angle
-function getMoonPhaseAngle(date) {
-    const year = date.getUTCFullYear();
-    const month = date.getUTCMonth() + 1; // months are 0-indexed
-    const day = date.getUTCDate() + (date.getUTCHours() / 24) + (date.getUTCMinutes() / 1440); // Include hours and minutes for smoothness
+
+// Calculate moon phase angle using datetime
+function getMoonPhaseAngle(datetime) {
+    const year = datetime.getUTCFullYear();
+    const month = datetime.getUTCMonth() + 1; // months are 0-indexed
+    const day = datetime.getUTCDate() + (datetime.getUTCHours() / 24) + (datetime.getUTCMinutes() / 1440); // Include hours and minutes for smoothness
 
     // Convert the date to Julian Days
     const jd = (367 * year) - Math.floor((7 * (year + Math.floor((month + 9) / 12))) / 4) + Math.floor((275 * month) / 9) + day + 1721013.5;
@@ -180,21 +194,22 @@ function getMoonPhaseAngle(date) {
     return moonPhaseAngle;
 }
 
-// Function to update the moon phase lighting
-function updateMoonPhase(date) {
-    const moonPhaseAngle = getMoonPhaseAngle(date);
+// Update the moon phase lighting
+function updateMoonPhase(datetime) {
+    const moonPhaseAngle = getMoonPhaseAngle(datetime);
 
     // Calculate the light position based on the moon phase angle
-    const lightX = -Math.sin(moonPhaseAngle) * 10; // Adjust distance as needed
+    const lightX = -Math.sin(moonPhaseAngle) * PHASE_LIGHT_SCALE;
     const lightY = 0; // Keep the light on the same plane as the moon for simplicity
-    const lightZ = Math.cos(moonPhaseAngle) * 10; // Adjust distance as needed
+    const lightZ = Math.cos(moonPhaseAngle) * PHASE_LIGHT_SCALE;
 
+    // Update lighting
     directionalLight.position.set(lightX, lightY, lightZ);
 }
 
-// Function to calculate the moon's libration and apparent size
-function calculateMoonLibration(date) {
-    const daysSinceJ2000 = (date - new Date(Date.UTC(2000, 0, 1, 12, 0, 0))) / (1000 * 60 * 60 * 24);
+// Calculate the moon's libration and apparent size
+function calculateMoonLibration(datetime) {
+    const daysSinceJ2000 = (datetime - new Date(Date.UTC(2000, 0, 1, 12, 0, 0))) / (1000 * 60 * 60 * 24);
 
     // Mean anomaly of the moon (degrees)
     const meanAnomaly = (134.963 + 13.064993 * daysSinceJ2000) % 360;
@@ -217,18 +232,18 @@ function calculateMoonLibration(date) {
     return { librationLongitude, librationLatitude, apparentSize };
 }
 
+// Update the current date by a given number of minutes
 function adjustDateTime(minutes) {
     currentDate = addMinutes(currentDate, minutes);
     document.getElementById('dateTimePicker').value = format(currentDate, "yyyy-MM-dd'T'HH:mm");
-    resetVisibility = true;
-    setResetVisibility();
+    disableResetButton(false);
     updateScene();
 }
 
-// Function to enable/disable reset button based on current state
-function setResetVisibility() {
+// Enable/disable the reset button
+function disableResetButton(disabled) {
     const resetButton = document.getElementById('resetButton');
-    resetButton.disabled = !resetVisibility;
+    resetButton.disabled = disabled;
 }
 
 // Update the moon's appeareance using rotation / libration / apparent size / phase based on current date
@@ -239,27 +254,33 @@ function updateScene() {
     moon.rotation.z = INITIAL_Z_ROTATION;
 
     const { librationLongitude, librationLatitude, apparentSize } = calculateMoonLibration(currentDate);
-    moon.rotation.x += -THREE.MathUtils.degToRad(librationLatitude * 3);
-    moon.rotation.y += THREE.MathUtils.degToRad(librationLongitude * 3);
+    moon.rotation.x += -THREE.MathUtils.degToRad(librationLatitude * LIBRATION_EFFECT);
+    moon.rotation.y += THREE.MathUtils.degToRad(librationLongitude * LIBRATION_EFFECT);
     moon.scale.setScalar(apparentSize / INITIAL_SCALE_FACTOR);
 }
 
-// Load shaders
-function loadShader(url) {
+// Load assets
+function loadTexture(path) {
     return new Promise((resolve, reject) => {
-        const loader = new THREE.FileLoader();
-        loader.load(url, (data) => resolve(data), undefined, reject);
+        textureLoader.load(path, (data) => resolve(data), undefined, reject);
+    });
+}
+function loadShader(path) {
+    return new Promise((resolve, reject) => {
+        fileLoader.load(path, (data) => resolve(data), undefined, reject);
     });
 }
 
-// Start initialization process after shaders are loaded
+// Main
 Promise.all([
-    loadShader('../shaders/vertex_shader.glsl'),
-    loadShader('../shaders/fragment_shader.glsl')
-]).then(([vertexShaderSource, fragmentShaderSource]) => {
-    const shaders = { vertexShaderSource, fragmentShaderSource };
-    init(shaders); // Call initialization function with loaded shaders
+    loadTexture(TEXTURE_MAP_PATH),
+    loadTexture(NORMAL_MAP_PATH),
+    loadShader(VERTEX_SHADER_PATH),
+    loadShader(FRAGMENT_SHADER_PATH)
+]).then(([textureMap, normalMap, vertexShader, fragmentShader]) => {
+    const assets = { textureMap, normalMap, vertexShader, fragmentShader };
+    init(assets); // First initialize with loaded textures + shaders
     animate(); // Start animation loop
 }).catch(error => {
-    console.error('Error loading shaders:', error);
+    console.error('Error loading assets:', error);
 });
